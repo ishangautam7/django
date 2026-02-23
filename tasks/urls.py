74b0b19d29a7cd6d603fde33a49085baad860a5f
Ishan Gautam <isangautam@gmail.com>
2026-02-23 17:43:35 +0545

    added task models

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('tasks/create/', views.task_create, name='task_create'),
+    path('tasks/<int:pk>/update/', views.task_update, name='task_update'),
+    path('tasks/<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import Task
 
 
 def home(request):
@@ -10,3 +11,42 @@ def home(request):
 def about(request):
     """About page view"""
     return render(request, 'tasks/about.html')
+
+
+# ---- CRUD Operations ----
+
+def task_list(request):
+    """READ - List all tasks"""
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def task_create(request):
+    """CREATE - Add a new task"""
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description', '')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/task_create.html')
+
+
+def task_update(request, pk):
+    """UPDATE - Edit an existing task"""
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description', '')
+        task.completed = request.POST.get('completed') == 'on'
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/task_update.html', {'task': task})
+
+
+def task_delete(request, pk):
+    """DELETE - Remove a task"""
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_delete.html', {'task': task})
